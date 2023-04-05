4392349bb45369fe805e87fd2db949e2eee3959a
Arina <118636926+lokijul341@users.noreply.github.com>
2023-04-05 12:10:00 +0300

    Добавлена документация в javadoc

---
+++ documentation/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.example.project_trpp","l":"ExampleInstrumentedTest"},{"p":"com.example.project_trpp","l":"ExampleUnitTest"},{"p":"com.example.project_trpp","l":"MainActivity"},{"p":"com.example.project_trpp","l":"WeightFragment.WeightDialogListener"},{"p":"com.example.project_trpp","l":"WeightFragment"}]

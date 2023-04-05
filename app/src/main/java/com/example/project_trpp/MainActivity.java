c4bbb0fd17d3283556ba87631e6f98ff9513a506
svetazhar7 <svetazhar7@gmail.com>
2023-04-05 11:53:45 +0300

    Добавлены javadoc комментарии

--- app/src/main/java/com/example/project_trpp/MainActivity.java
+++ app/src/main/java/com/example/project_trpp/MainActivity.java
@@ -1,5 +1,4 @@
 package com.example.project_trpp;
-
 import android.annotation.SuppressLint;
 import android.content.SharedPreferences;
 import android.widget.ImageButton;
@@ -7,29 +6,63 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
+
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
+
+/**
+
+ The MainActivity class is responsible for controlling the main activity of the application.
+ It contains methods for calculating water goal, tracking water intake and displaying weight dialog.
+ @author  Zharkova Svetlana, Skityova Anastasia
+ @version 1.0
+ @since 2023-04-05
+ */
 public class MainActivity extends AppCompatActivity implements WeightFragment.WeightDialogListener {
+    /**
 
+     The amount of water that has been consumed.
+     */
     private int waterAmount = 0;
-    private int weight = 0;
+    /**
 
+     The user's weight, used to calculate the recommended daily water intake.
+     */
+    private int weight = 0;
+    /**
+
+     The TextView that displays the user's recommended daily water intake.
+     */
     private TextView waterGoalTextView;
+    /**
+
+     The TextView that displays the amount of water that has been consumed.
+     */
     private TextView waterAmountTextView;
+    /**
+
+     The ImageButton that opens the weight input dialog.
+     */
     private ImageButton weightButton;
+    /**
+
+     The ImageButton that increments the amount of water consumed.
+     */
     private ImageButton drinkButton;
 
     private SharedPreferences sharedPreferences;
 
+    /**
+     * Called when the activity is starting. Responsible for initializing the activity.
+     * Sets up the UI elements and retrieves the stored weight value from SharedPreferences.
+     *
+     * @param savedInstanceState the bundle containing the activity's previously saved state
+     */
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         waterGoalTextView = findViewById(R.id.water_goal_text_view);
         waterAmountTextView = findViewById(R.id.water_amount_text_view);
         weightButton = findViewById(R.id.weight_button);
@@ -59,22 +92,36 @@ public class MainActivity extends AppCompatActivity implements WeightFragment.We
         });
     }
 
+    /**
+     * Calculates the daily water goal based on the user's weight and updates the UI.
+     */
     private void calculateWaterGoal() {
         int waterGoal = weight * 50;
         waterGoalTextView.setText(waterGoal + " мл");
     }
 
+    /**
+     * Increases the water amount by 250 mL and updates the UI.
+     */
     private void drinkWater() {
         waterAmount += 250;
         waterAmountTextView.setText(waterAmount + " мл");
     }
 
+    /**
+     * Shows the weight dialog to the user.
+     */
     private void showWeightDialog() {
         WeightFragment weightFragment = new WeightFragment();
         weightFragment.show(getSupportFragmentManager(), "weightDialog");
     }
 
-
+    /**
+     * Called when the user enters their weight in the weight dialog.
+     * Updates the weight value, calculates the water goal and stores the weight in SharedPreferences.
+     *
+     * @param weight the weight entered by the user
+     */
     @Override
     public void onWeightEntered(int weight) {
         this.weight = weight;
@@ -83,4 +130,5 @@ public class MainActivity extends AppCompatActivity implements WeightFragment.We
         editor.putInt("weight", weight);
         editor.apply();
     }
+
 }
--- app/src/main/java/com/example/project_trpp/WeightFragment.java
+++ app/src/main/java/com/example/project_trpp/WeightFragment.java
@@ -11,15 +11,49 @@ import android.widget.Toast;
 
 import androidx.fragment.app.DialogFragment;
 
+/**
+
+ A dialog fragment used to prompt the user to input their weight.
+ */
 public class WeightFragment extends DialogFragment {
 
-    private EditText weightEditText;
-    private Button saveButton;
+    /**
 
+     The EditText used to input the user's weight.
+     */
+    private EditText weightEditText;
+    /**
+
+     The button used to save the user's weight.
+     */
+    private Button saveButton;
+    /**
+
+     Interface definition for a callback to be invoked when the user enters their weight.
+     */
     public interface WeightDialogListener {
+
+        /**
+
+         Called when the user has entered their weight.
+         @param weight The weight entered by the user.
+         */
         void onWeightEntered(int weight);
     }
+    /**
 
+     Called to have the fragment instantiate its user interface view. This method is
+
+     called between onCreate(Bundle) and onActivityCreated(Bundle).
+
+     @param inflater The LayoutInflater object that can be used to inflate any views in the fragment.
+
+     @param container If non-null, this is the parent view that the fragment's UI should be attached to.
+
+     @param savedInstanceState If non-null, this fragment is being re-constructed from a previous saved state as given here.
+
+     @return Return the View for the fragment's UI, or null.
+     */
     @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -44,10 +78,14 @@ public class WeightFragment extends DialogFragment {
 
         return view;
     }
+
+    /**
+
+     This method will be called when the fragment is visible and in focus.
+     */
     @Override
     public void onResume() {
-        super.onResume();
-        // Установить ширину и высоту окна фрагмента
+
         int width = getResources().getDimensionPixelSize(R.dimen.dialog_weight_width);
         int height = getResources().getDimensionPixelSize(R.dimen.dialog_weight_height);
         getDialog().getWindow().setLayout(width, height);
